3fd8fe051cd7e2eda38af1feebb35c535e9f57e7
준화 <junwha0511@naver.com>
2020-09-10 11:15:39 +0900

    Update 1629.py

--- 1629.py
+++ 1629.py
@@ -2,10 +2,11 @@ def powC(a, b, c):
     if b == 0:
         return 1
     
+    result = powC(a, b//2, c)**2
     if b%2 == 0:
-        return powC(a, b//2, c)*powC(a, b//2, c) % c
+        return result % c
     else:
-        return powC(a, b//2, c)*powC(a, b//2, c)*a %c
+        return result*a %c
 
 A, B, C = map(int, input().split(" "))
 print(powC(A, B, C))

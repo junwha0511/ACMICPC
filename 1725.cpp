9504a65d62128ab084d0f3d5bbfa1e8b4fb52bf5
준화 <junwha0511@naver.com>
2021-01-29 23:14:27 +0900

    1725 UNSOLVED

---
+++ 1725.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+typedef long long lld;
+
+lld N, h[101010];
+
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio();
+
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> h[i];
+    }
+}
+

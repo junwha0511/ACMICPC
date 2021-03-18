53fdfaf525d0e4e847ac006b1f2674ff5a7fce18
준화 <junwha0511@naver.com>
2021-03-18 18:27:02 +0900

    Create 17608.cpp

---
+++ 17608.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    
+    int arr[101010];
+
+    for(int i=0; i<N; i++){
+        cin >> arr[i];    
+    }
+
+    int max = arr[N-1];
+    int cnt = 1;
+
+    for(int i=N-2; i>=0; i--){
+        if(arr[N-1] > max){
+            max = arr[N-1];
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+}

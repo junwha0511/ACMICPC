74199e6d06cc9c0baa1607dcc84dd3cbe1aeb232
준화 <junwha0511@naver.com>
2023-03-23 00:13:49 +0900

    2751 SOLVED

--- 2751.py
+++ 2751.py
@@ -1,5 +1,34 @@
-n = int(input())
-numbers = []
-[numbers.append(int(input())) for i in range(n)]
-numbers.sort()
-[print(i) for i in numbers]
+N = int(input())
+arr = [int(input()) for i in range(N)]
+
+def merge_sort(arr, start, end):
+    if end - start <= 1:
+        return
+    mid = (start+end)//2
+    merge_sort(arr, start, mid)
+    merge_sort(arr, mid, end)
+    
+    left = arr[start:mid].copy()
+    right = arr[mid:end].copy()
+    i, j, k = 0, 0, start
+    
+    while k < end:
+        if i == len(left):
+            arr[k] = right[j]
+            j += 1
+        elif j == len(right):
+            arr[k] = left[i]
+            i += 1
+        else:
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+        k += 1
+
+merge_sort(arr, 0, N)
+
+for a in arr:
+    print(a)

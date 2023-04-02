4c960ba5a99b532ee1814810ffcde249973b3587
준화 <junwha0511@naver.com>
2023-04-03 02:13:12 +0900

    2749 SOLVED - using pisano period

--- 2749.py
+++ 2749.py
@@ -4,37 +4,21 @@ Naive: typical fibo
 000000000 => O(N)도 1000000000초
 O(logN)은 필요함
 모듈로 1000000000에서 줄일 수 있는 방법을 찾아야
-0 1 1 2 3 5
-f[n] = f[2]*f[n-1] + f[1]*f[n-2]
-= f[3]*f[n-2] + f[2]*f[n-3]
-= f[4]*f[n-3] + f[3]*f[n-4]
-= f[5]*f[n-4] + f[4]*f[n-5]
-= f[6]*f[n-5] + f[5]*f[n-6]
-...
-= f[k]*f[n-(k-1)] + f[k-1]*f[n-k]
 
-=> k를 정하면 log_k(N)만에 계산이 가능하다! (k씩 건너 뛰기)
+https://math.stackexchange.com/questions/3718940/logic-behind-pisano-period
+(Proof by pigeonhole principle)
+Pi(n) for 10^k = 15*10^(k-1)
 
-hmm
 '''
-
 N = int(input())
-P = 1000000
-K = 1000000
-f = [None for _ in range(K+1)]
+M = 10**6
+P = 15*10**5
+
+f = [None for _ in range(P+1)]
 f[0] = 0
 f[1] = 1
 
-for i in range(2, K+1):
-    f[i] = (f[i-1] + f[i-2]) % P
+for i in range(2, P+1):
+    f[i] = (f[i-1] + f[i-2]) % M
 
-q = [(1, N)]
-result = 0
-while len(q) > 0:
-    c, n = q.pop()
-    if n <= K:
-        result = (result + ((c*f[n]) % P)) % P
-    else:
-        q.append(((c*f[K])%P, n-(K-1)))
-        q.append(((c*f[K-1])%P, n-K))
-print(result)
+print(f[N%P])
